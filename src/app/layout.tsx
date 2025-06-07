68950c3f7044aafed392be5657302bdf82301e78
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-06-07 10:47:01 +0100

    Correcting layout for small screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="w-full h-3 bg-blue-900"></div>
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="w-full max-w-[1400px] mx-auto justify-self-center p-10 items-center md:px-20">
           <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
             {children}
           </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,11 @@ import LinkDoar from "@/components/LinkDoar";
 export default function Home() {
   return (
     <div className="flex flex-col items-center gap-6">
-      <div className="flex items-center">
+      <div className="flex flex-col md:flex-row items-center justify-center">
         <HeartThanks />
-        <h1 className="text-4xl font-bold">Faça bater um coração</h1>
+        <h1 className="text-2xl font-bold md:text-4xl">
+          Faça bater um coração
+        </h1>
       </div>
       <LinkDoar />
     </div>
