21d57e30f7a0929d50965204d296d67d04f41333
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-06-10 09:01:36 +0100

    Correction on the db connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,18 +1,24 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI as string;
-
-if (!uri) {
-  throw new Error("MONGODB_URI não definida");
-}
-
-const client = new MongoClient(uri);
-
 export async function getHeartNames() {
-  await client.connect();
+  const uri = process.env.MONGODB_URI as string;
+
+  if (!uri) {
+    throw new Error("MONGODB_URI não definida");
+  }
+
+  const client = new MongoClient(uri);
+
+  if (process.env.NODE_ENV === "production" && typeof window === "undefined") {
+    await client.connect();
+  }
+
   const db = client.db("mongodb");
   const collection = db.collection("doar-casadamiao");
-
-  const result = await collection.find().sort({ date: -1 }).toArray();
-  return result;
+  try {
+    const result = await collection.find().sort({ date: -1 }).toArray();
+    return result;
+  } catch {
+    return [];
+  }
 }
